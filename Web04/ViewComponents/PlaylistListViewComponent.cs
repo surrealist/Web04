8e045c80db6a2503cc9b09806c0a850b9490f8d2
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2017-12-24 14:00:06 +0700

    completing Playlist menu
    Can display playlist and its music items (Song and Album)

---
+++ Web04/ViewComponents/PlaylistListViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Web04.Data;
+using Web04.Models;
+
+namespace Web04.ViewComponents {
+
+  public class PlaylistListViewComponent : ViewComponent {
+
+    private readonly ApplicationDbContext db;
+    private readonly UserManager<ApplicationUser> userManager;
+
+    public PlaylistListViewComponent(ApplicationDbContext db,
+        UserManager<ApplicationUser> userManager) {
+      this.db = db;
+      this.userManager = userManager;       
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync() {
+      var currentUser = await userManager.FindByEmailAsync(User.Identity.Name);
+
+      var q = from p in db.Playlists
+              where p.Owner == currentUser
+              select p;
+
+      var items = await q.ToListAsync();
+      foreach(var item in items) {
+        db.Entry(item).Collection(x => x.Items).Load();
+      }
+      return View(await q.ToListAsync());
+    }
+  }
+}
---
+++ Web04/ViewComponents/PlaylistViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web04.Models;
+
+namespace Web04.ViewComponents {
+  public class PlaylistViewComponent : ViewComponent {
+
+    public IViewComponentResult Invoke(Playlist playlist) {
+      return View(playlist);
+    }
+
+  }
+}
